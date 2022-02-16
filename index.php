5a72fc7c030f6a37b28af762ebcc864be5e3606d
unknown <contatoluiggibrito04@gmail.com>
2022-02-16 09:02:17 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>revisão</title>
+</head>
+<body>
+    <?php
+       $nome = 'Eliton';
+       $idade = 33;
+       $salario = 75999.70;
+
+       $user = [
+        'nome' => $nome,
+       'idade' => $idade,
+       'salario' => $salario
+
+       ];
+              
+          var_dump(
+              $nome,
+              $idade,
+              $salario
+          );
+?>
+</body>
+</html>
